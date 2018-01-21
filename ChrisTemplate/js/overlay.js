00972380851d15066ed0ecdf94e3cace60b83e08
mic739 <miboem@googlemail.com>
2018-01-21 12:45:02 +0100

    a

--- ChrisTemplate/js/overlay.js
+++ ChrisTemplate/js/overlay.js
@@ -1,16 +1,16 @@
 function on() {
     document.getElementById("overlay").style.display = "block";
-    var wall = new Freewall("#freewall");
-    wall.reset({
-        selector: '.brick',
-        animate: true,
-        cellW: 160,
-        cellH: 160,
-        delay: 50,
-        onResize: function() {
-            wall.fitWidth();
-        }
-    });
+    // var wall = new Freewall("#freewall");
+    // wall.reset({
+    //     selector: '.brick',
+    //     animate: true,
+    //     cellW: 160,
+    //     cellH: 160,
+    //     delay: 50,
+    //     onResize: function() {
+    //         wall.fitWidth();
+    //     }
+    // });
 }
 
 function off() {
